68b657bf00b29b3b681fb1a5d4a0b12d06a3527a
Darshan <darshansanghavi01@gmai.com>
2025-07-23 21:45:09 +0530

    Home Page Made Expect Heart Icon

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -22,25 +22,28 @@ import ProductList from './pages/Admin/ProductList.jsx';
 import ProductUpdate from './pages/Admin/ProductUpdate.jsx';
 import AllProducts from './pages/Admin/AllProducts.jsx';
 
+//Pages
+import Home from './pages/Home.jsx';
+
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<App />}>                                //parent route
       
       <Route path='/login' element={<Login />} />                     //child route
-      <Route path='/register' element={<Register />} />               //child route 
+      <Route path='/register' element={<Register />} />               
+      <Route index={true} path='/' element={<Home />} />              
 
       <Route path='' element={<PrivateRoute />}>                     //private route 
         <Route path='/profile' element={<Profile/>}/>                //child route of private route          
       </Route> 
 
-      //AdminRoute
       <Route path='/admin' element={<AdminRoute />}>                            //admin routes
         <Route path='/admin/userlist' element={<UserList />} />                 //child route of admin route
         <Route path='/admin/categorylist' element={<CategoryList/>} />
-        <Route path='/admin/productlist' element={<ProductList />} />           //child route of admin route
-        <Route path='/admin/product/update/:id' element={<ProductUpdate />} />  //child route of admin route
-        <Route path='/admin/allproductslist' element={<AllProducts />} />           //child route of admin route
+        <Route path='/admin/productlist' element={<ProductList />} />           
+        <Route path='/admin/product/update/:id' element={<ProductUpdate />} />  
+        <Route path='/admin/allproductslist' element={<AllProducts />} />           
       </Route>
 
     </Route >
