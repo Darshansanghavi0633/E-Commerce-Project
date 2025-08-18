d329c1a014ffa0ad0ca38bc9fd0d832378b28df4
Darshan <darshansanghavi01@gmai.com>
2025-08-18 21:47:58 +0530

    Backend  Completed

--- Backend/routes/orderRoutes.js
+++ Backend/routes/orderRoutes.js
@@ -1,8 +1,30 @@
 import express from "express";
-import  {authenticateUser,authorizeAdmin} from "../middlewares/authMiddleware.js";
-import { createOrder } from './../controllers/orderController.js';
 const router = express.Router();
 
-router.route("/").post(authenticateUser,createOrder);
+import {
+  createOrder,
+  getAllOrders,
+  getUserOrders,
+  countTotalOrders,
+  calculateTotalSales,
+  calcualteTotalSalesByDate,
+  findOrderById,
+  markOrderAsPaid,
+  markOrderAsDelivered,
+} from "../controllers/orderController.js";
+
+import { authenticateUser, authorizeAdmin } from "../middlewares/authMiddleware.js";
+
+router.route("/")
+  .post(authenticateUser, createOrder)
+  .get(authenticateUser, authorizeAdmin, getAllOrders);
+
+router.route("/mine").get(authenticateUser, getUserOrders);
+router.route("/total-orders").get(countTotalOrders);
+router.route("/total-sales").get(calculateTotalSales);
+router.route("/total-sales-by-date").get(calcualteTotalSalesByDate);
+router.route("/:id").get(authenticateUser, findOrderById);
+router.route("/:id/pay").put(authenticateUser, markOrderAsPaid);
+router.route("/:id/deliver").put(authenticateUser, authorizeAdmin, markOrderAsDelivered);
 
 export default router;
